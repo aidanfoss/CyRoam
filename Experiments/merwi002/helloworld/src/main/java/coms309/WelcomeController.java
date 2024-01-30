9c74d8a46eb5f849e07baea287139fca4f106889
Zach Merwin <merwi002@iastate.edu>
2024-01-30 13:05:22 -0600

    first commit

---
+++ Experiments/merwi002/helloworld/src/main/java/coms309/WelcomeController.java
@@ -0,0 +1,19 @@
+package coms309;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+class WelcomeController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Hello and welcome to COMS 309 CySplore will be the best App!!!";
+    }
+
+    @GetMapping("/{name}")
+    public String welcome(@PathVariable String name) {
+        return "Hello and welcome to COMS 309, this person is the best------> " + name;
+    }
+}
