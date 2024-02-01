9b16ee329db769ecc0bd89ea9bfaabe93d897b3c
Aidan Foss <quantumaidan@gmail.com>
2024-02-01 15:50:07 -0600

    added image, changed text, centered text and changed font size. Attempted adding zach button and zach activity. Seems to crash instantly.

--- Experiments/awfoss/exp1/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiments/awfoss/exp1/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -2,19 +2,19 @@ package com.example.androidexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+//import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+//import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView messageText;   // define message textview variable
-
+    private Button zachButton;
    // @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
         //for (int i = 0; i < 100; i++){ adds all 100 ".'s" before ever displaying
         //while(true){ never displays text
-            name += ".";
+            name += "'s Lab from last semester";
             messageText.setText(name);
             /*
             try {
@@ -36,6 +36,15 @@ public class MainActivity extends AppCompatActivity {
             */
 
         //}
+        zachButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* when zach button is pressed, use intent to switch to ZachM Activity */
+                Intent intent = new Intent(MainActivity.this, ZachMActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
