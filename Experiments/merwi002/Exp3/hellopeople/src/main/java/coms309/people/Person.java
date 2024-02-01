a7cdff4bb395e5785b6cc86d4f5eb4419052ca21
Zach Merwin <merwinzach1@gmail.com>
2024-02-01 14:01:01 -0600

    Exp3 added, dabbles in passwords

---
+++ Experiments/merwi002/Exp3/hellopeople/src/main/java/coms309/people/Person.java
@@ -0,0 +1,94 @@
+package coms309.people;
+
+
+/**
+ * Provides the Definition/Structure for the people row
+ *
+ * @author Vivek Bengre
+ */
+
+public class Person {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String address;
+
+    private String telephone;
+
+    private String points;
+
+    private String password;
+
+
+    public Person(){
+        
+    }
+
+    public Person(String firstName, String lastName, String address, String telephone, String points,String password ){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.telephone = telephone;
+        this.points = points;
+        this.password = password;
+
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return this.telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+    public String getPoints() {
+        return this.points;
+    }
+
+    public void setPoints(String points) {
+        this.points = points;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " 
+               + lastName + " "
+               + address + " "
+               + telephone + " "
+               + points;
+    }
+}
