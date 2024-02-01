b086348a4e9a7fb954bd0f6cf8bacb9b3cd54adc
Aidan Foss <quantumaidan@gmail.com>
2024-02-01 14:35:29 -0600

    added a map to the login test, which creates a marker on central campus.

--- Experiments/awfoss/exp3/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiments/awfoss/exp3/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView messageText;   // define message textview variable
     private TextView usernameText;  // define username textview variable
+
+    private Button showMap;
     private Button loginButton;     // define login button variable
     private Button signupButton;    // define signup button variable
 
@@ -25,8 +27,18 @@ public class MainActivity extends AppCompatActivity {
         /* initialize UI elements */
         messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
         usernameText = findViewById(R.id.main_username_txt);// link to username textview in the Main activity XML
+        showMap = findViewById(R.id.showMap);
         loginButton = findViewById(R.id.main_login_btn);    // link to login button in the Main activity XML
         signupButton = findViewById(R.id.main_signup_btn);  // link to signup button in the Main activity XML
+
+        showMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
         /* extract data passed into this activity from another activity */
         Bundle extras = getIntent().getExtras();
@@ -36,8 +48,8 @@ public class MainActivity extends AppCompatActivity {
         } else {
             messageText.setText("Welcome");
             usernameText.setText(extras.getString("USERNAME")); // this will come from LoginActivity
-            loginButton.setVisibility(View.INVISIBLE);              // set login button invisible
-            signupButton.setVisibility(View.INVISIBLE);             // set signup button invisible
+            //loginButton.setVisibility(View.INVISIBLE);              // set login button invisible
+            //signupButton.setVisibility(View.INVISIBLE);             // set signup button invisible
         }
 
         /* click listener on login button pressed */
@@ -46,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 /* when login button is pressed, use intent to switch to Login Activity */
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+               Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
