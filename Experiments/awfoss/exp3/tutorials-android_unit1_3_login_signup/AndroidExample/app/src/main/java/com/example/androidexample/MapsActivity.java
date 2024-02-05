d723d0e086bdb3af0923daae4249d9717dccfd80
Aidan Foss <quantumaidan@gmail.com>
2024-02-05 12:36:36 -0600

    added updateLocationUI(), trying to establish live tracking of the user

--- Experiments/awfoss/exp3/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/MapsActivity.java
+++ Experiments/awfoss/exp3/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/MapsActivity.java
@@ -6,6 +6,7 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.FrameLayout;
 
 import com.google.android.gms.maps.CameraUpdate;
@@ -15,6 +16,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Objects;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -36,10 +39,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         this.gMap = googleMap;
+        updateLocationUI();
         LatLng mapIowaState = new LatLng(42.023949, -93.647595);
         this.gMap.addMarker(new MarkerOptions().position(mapIowaState).title("Marker At College"));
         this.gMap.moveCamera(CameraUpdateFactory.zoomTo(13));
         this.gMap.moveCamera(CameraUpdateFactory.newLatLng(mapIowaState));
+//        this.gMap.
 
+
+//this doesnt belong INSIDE onMapReady, but it needs to call gMap, so i need to figure out how to use gMap
+// outside of the onMapReady function
+        private void updateLocationUI() { //got this chunk of code from https://developers.google.com/maps/documentation/android-sdk/current-place-tutorial#java_4
+            //changed map call to gMap call, not sure how to check for locationPermissionGranted or lastKnownLocation just yet.
+            //did i miss something in the documentation? This entire function is in the wrong spot.
+            if (gMap == null) {
+                return;
+            }
+            try {
+                if (locationPermissionGranted) {
+                    gMap.setMyLocationEnabled(true);
+                    gMap.getUiSettings().setMyLocationButtonEnabled(true);
+                } else {
+                    gMap.setMyLocationEnabled(false);
+                    gMap.getUiSettings().setMyLocationButtonEnabled(false);
+                    lastKnownLocation = null;
+                    getLocationPermission();
+                }
+            } catch (SecurityException e)  {
+                Log.e("Exception: %s", Objects.requireNonNull(e.getMessage()));
+            }
+        }
     }
+
+
 }
