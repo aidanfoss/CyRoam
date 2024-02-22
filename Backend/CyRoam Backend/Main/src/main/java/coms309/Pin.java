2b7a29b8c2a4b3f29a73035d6a739373e9f5bb21
Caleb <caleb.iddings@icloud.com>
2024-02-22 17:34:24 -0600

    Started making a pin class

--- Backend/CyRoam Backend/Main/src/main/java/coms309/Pin.java
+++ Backend/CyRoam Backend/Main/src/main/java/coms309/Pin.java
@@ -1,6 +1,56 @@
 package coms309;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import com.fasterxml.jackson.annotation.JsonTypeId;
+
+@Entity
 public class Pin {
-    int latitude;
-    int longitude;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private double x;
+    private double y;
+    private String name;
+
+    public Pin(double x, double y, String name) {
+        this.x = x;
+        this.y = y;
+        this.name = name;
+    }
+
+    public Pin() {
+    }
+
+    public void setX(double x) {
+        this.x = x;
+        return;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+        return;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
