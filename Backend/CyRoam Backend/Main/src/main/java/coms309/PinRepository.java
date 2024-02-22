0f36b2a1bbabad1f7dd73d735c3080d9f6421b4a
Caleb <caleb.iddings@icloud.com>
2024-02-22 17:34:24 -0600

    Started making a pin class

---
+++ Backend/CyRoam Backend/Main/src/main/java/coms309/PinRepository.java
@@ -0,0 +1,11 @@
+package coms309;
+
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PinRepository extends JpaRepository<Pin, Long> {
+    Pin findById(int id);
+
+    @Transactional
+    void deleteById(int id);
+}
