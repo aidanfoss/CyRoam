d170e7b0e90c69c15a07f869f7756ebcda06bd2f
Nick127k <Nick127k@iastate.edu>
2024-02-24 14:50:20 -0600

    semimade(but needs to be coded) Login Input activity
    Signed-off-by: Nick127k <Nick127k@iastate.edu>

--- Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/LoginInputActivity.java
+++ Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/LoginInputActivity.java
@@ -2,7 +2,9 @@ package com.lg1_1.cyroam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class LoginInputActivity extends AppCompatActivity {
@@ -12,5 +14,12 @@ public class LoginInputActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_input);
         Button loginButton = findViewById(R.id.Loginbutton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginInputActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
