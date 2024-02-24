fd0c0c9906d4ee65e0502b26b60ca5f909516e64
Nick127k <Nick127k@iastate.edu>
2024-02-24 14:43:11 -0600

    Creating Login Input activity
    Signed-off-by: Nick127k <Nick127k@iastate.edu>

--- Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/LoginInputActivity.java
+++ Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/LoginInputActivity.java
@@ -3,6 +3,7 @@ package com.lg1_1.cyroam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginInputActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class LoginInputActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_input);
+        Button loginButton = findViewById(R.id.Loginbutton);
     }
 }
