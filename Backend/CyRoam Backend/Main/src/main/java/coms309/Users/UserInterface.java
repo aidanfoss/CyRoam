662e74d68a29a0b90a50365ee6151f2a0436723c
Zach Merwin <merwi002@iastate.edu>
2024-02-24 19:23:06 -0600

    set up User class on friends table branch using h2

---
+++ Backend/CyRoam Backend/Main/src/main/java/coms309/Users/UserInterface.java
@@ -0,0 +1,20 @@
+package coms309.Users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author Zach Merwin
+ *
+ */
+
+public interface UserInterface extends JpaRepository<User, Long> {
+
+    User findByUId(int uId);
+
+    void deleteById(int uId);
+
+    User findByLaptop_Id(int uId);
+}
+
