542db50829f59ec9ef2327cb10aec442fbe42bff
Nick127k <Nick127k@iastate.edu>
2024-02-25 14:57:00 -0600

    Friend Screen ruff draft lol is finished
    Signed-off-by: Nick127k <Nick127k@iastate.edu>

--- Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/FriendActivity.java
+++ Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/FriendActivity.java
@@ -6,11 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class FriendActivity extends AppCompatActivity {
     private Button backbutton;
 
     private Button friendsearch;
+
+    private boolean found = false;
+
+    private TextView outputtext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +23,7 @@ public class FriendActivity extends AppCompatActivity {
 
         backbutton = findViewById(R.id.Backbutton);
         friendsearch =  findViewById(R.id.searchButton);
+        outputtext = findViewById(R.id.Outputtext);
 
 
         backbutton.setOnClickListener(new View.OnClickListener() {
@@ -32,8 +38,12 @@ public class FriendActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 /* when signup button is pressed, use intent to switch to Signup Activity */
-                Intent intent = new Intent(FriendActivity.this, MainActivity.class);
-                startActivity(intent);  // go to SignupActivity
+                if(found = false){
+                    outputtext.setText("User doesnot exist");
+                }
+                else{
+                    outputtext.setText("Invite sent");
+                }
             }
         });
     }
