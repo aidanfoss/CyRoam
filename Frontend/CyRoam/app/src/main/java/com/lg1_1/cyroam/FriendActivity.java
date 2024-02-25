7a3f66af5897d62730267223f3d241ffb04fe8d1
Nick127k <Nick127k@iastate.edu>
2024-02-24 22:06:10 -0600

    Friend Screen under development
    Signed-off-by: Nick127k <Nick127k@iastate.edu>

---
+++ Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/FriendActivity.java
@@ -0,0 +1,38 @@
+package com.lg1_1.cyroam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FriendActivity extends AppCompatActivity {
+    private Button backbutton;
+
+    private Button friendsearch;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend);
+
+        backbutton = findViewById(R.id.Backbutton);
+        friendsearch =  findViewById(R.id.searchButton);
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* when signup button is pressed, use intent to switch to Signup Activity */
+                Intent intent = new Intent(FriendActivity.this, MainActivity.class);
+                startActivity(intent);  // go to SignupActivity
+            }
+        });
+        friendsearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* when signup button is pressed, use intent to switch to Signup Activity */
+                Intent intent = new Intent(FriendActivity.this, MainActivity.class);
+                startActivity(intent);  // go to SignupActivity
+            }
+        });
+    }
+}
