b6f285fe8cbd18f333968a0c355167fe7d3430cf
Nick127k <Nick127k@iastate.edu>
2024-02-24 19:24:18 -0600

    adding the makeStringReq
    Signed-off-by: Nick127k <Nick127k@iastate.edu>

--- Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/LoginInputActivity.java
+++ Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/LoginInputActivity.java
@@ -4,10 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
+
+
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginInputActivity extends AppCompatActivity {
+    private static final String URL_STRING_REQ = "https://jsonplaceholder.typicode.com/users/1";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +33,52 @@ public class LoginInputActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginInputActivity.this, MapsActivity.class);
-                startActivity(intent);
+                makeStringReq();
+                //Intent intent = new Intent(LoginInputActivity.this, MapsActivity.class);
+                //startActivity(intent);
             }
         });
     }
+    private void makeStringReq() {
+
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.GET,
+                URL_STRING_REQ,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Handle the successful response here
+                        Log.d("Volley Response", response);
+                        //msgResponse.setText(response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle any errors that occur during the request
+                        Log.e("Volley Error", error.toString());
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+//                headers.put("Authorization", "Bearer YOUR_ACCESS_TOKEN");
+//                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+//                params.put("param1", "value1");
+//                params.put("param2", "value2");
+                return params;
+            }
+        };
+
+        // Adding request to request queue
+        //VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);
+    }
+
 }
