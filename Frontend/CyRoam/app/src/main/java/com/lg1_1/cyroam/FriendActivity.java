d1adb7b2bc1616ef631ed9dc5abaf37a6d9aac54
Nick127k <Nick127k@iastate.edu>
2024-02-25 19:42:18 -0600

    Adding string req
    Signed-off-by: Nick127k <Nick127k@iastate.edu>

--- Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/FriendActivity.java
+++ Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/FriendActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class FriendActivity extends AppCompatActivity {
+    private static final String URL_STRING_REQ = "https://jsonplaceholder.typicode.com/users/1";
     private Button backbutton;
 
     private Button friendsearch;
@@ -46,5 +56,46 @@ public class FriendActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+    private void makeStringReq() {
+
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.GET,
+                URL_STRING_REQ,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Handle the successful response here
+                        Log.d("Volley Response", response);
+                        //msgResponse.setText(response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle any errors that occur during the request
+                        Log.e("Volley Error", error.toString());
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+//                headers.put("Authorization", "Bearer YOUR_ACCESS_TOKEN");
+//                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+//                params.put("param1", "value1");
+//                params.put("param2", "value2");
+                return params;
+            }
+        };
+
+        // Adding request to request queue
+        //VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);
     }
 }
--- Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/SignupActivity.java
+++ Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/SignupActivity.java
@@ -4,13 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class SignupActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignupActivity extends AppCompatActivity {
+    private static final String URL_STRING_REQ = "https://jsonplaceholder.typicode.com/users/1";
     private EditText usernameEditText;  // define username edittext variable
     private EditText passwordEditText;  // define password edittext variable
     private EditText confirmEditText;   // define confirm edittext variable
@@ -52,11 +61,55 @@ public class SignupActivity extends AppCompatActivity {
 
                 if (password.equals(confirm)){
                     Toast.makeText(getApplicationContext(), "Signing up", Toast.LENGTH_LONG).show();
+                    makeStringReq();
+                    Intent intent = new Intent(SignupActivity.this, FriendActivity.class);
+                    startActivity(intent);
                 }
                 else {
                     Toast.makeText(getApplicationContext(), "Password don't match", Toast.LENGTH_LONG).show();
                 }
             }
         });
+    }
+    private void makeStringReq() {
+
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.GET,
+                URL_STRING_REQ,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Handle the successful response here
+                        Log.d("Volley Response", response);
+                        //msgResponse.setText(response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle any errors that occur during the request
+                        Log.e("Volley Error", error.toString());
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+//                headers.put("Authorization", "Bearer YOUR_ACCESS_TOKEN");
+//                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+//                params.put("param1", "value1");
+//                params.put("param2", "value2");
+                return params;
+            }
+        };
+
+        // Adding request to request queue
+        //VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);
     }
 }
