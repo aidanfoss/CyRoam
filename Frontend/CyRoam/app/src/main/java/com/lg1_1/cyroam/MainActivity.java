97ac0892d8ad3216f6f9f6ac794bee9d707c888e
Aidan Foss <quantumaidan@gmail.com>
2024-02-26 19:52:11 -0600

    re-added the username password editText boxes. I noticed nick had a seperate XML for the login screen, so i took the code from there. Renamed editTextTextUsername to editTextUsername. Ditto for editTextTextPassword

--- Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/MainActivity.java
+++ Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/MainActivity.java
@@ -1,3 +1,10 @@
+/*
+this activity is solely to request permissions and establish internet connection.
+after this, moves directly to the login screen, which can bring you to the signup screen
+if needed. For now, the login screen doesnt require a valid login.
+ */
+
+
 package com.lg1_1.cyroam;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +22,10 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView messageText;   // define message textview variable
-    private TextView usernameText;  // define username textview variable
+//    private TextView messageText;   // define message textview variable
+//    private TextView usernameText;  // define username textview variable
 
-    private Button showMap;
+//    private Button showMap;
 //    private Button loginButton;     // define login button variable
 //    private Button signupButton;    // define signup button variable
 
@@ -28,10 +35,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);             // link to Main activity XML
 
         /* initialize UI elements */
-        messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
-        usernameText = findViewById(R.id.main_username_txt);// link to username textview in the Main activity XML
-        showMap = findViewById(R.id.showMap);
-        
+        //messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
+        //usernameText = findViewById(R.id.main_username_txt);// link to username textview in the Main activity XML
+        //showMap = findViewById(R.id.showMap);
 
 
         //TODO correctly ask for location permissions on load
@@ -55,40 +61,30 @@ public class MainActivity extends AppCompatActivity {
 //                  if uncommented, would be broken and unusable. https://developers.google.com/maps/documentation/android-sdk/current-place-tutorial#java_4
 
 
-
-        //TODO move showmap listener to login screen
-        showMap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
-                startActivity(intent);
-            }
-        });
-
         //leftover useful code. Used again in MapsActivity to pass new pin data from the new pin activity
-        Bundle extras = getIntent().getExtras();
-        if(extras == null) {
-            messageText.setText("Home Page");
-            usernameText.setVisibility(View.INVISIBLE);             // set username text invisible initially
-        } else {
-            messageText.setText("Welcome");
-            usernameText.setText(extras.getString("USERNAME")); // this will come from LoginActivity
-            //loginButton.setVisibility(View.INVISIBLE);              // set login button invisible
-            //signupButton.setVisibility(View.INVISIBLE);             // set signup button invisible
-        }
+//        Bundle extras = getIntent().getExtras();
+//        if(extras == null) {
+//            messageText.setText("Home Page");
+//            usernameText.setVisibility(View.INVISIBLE);             // set username text invisible initially
+//        } else {
+//            messageText.setText("Welcome");
+//            usernameText.setText(extras.getString("USERNAME")); // this will come from LoginActivity
+//            //loginButton.setVisibility(View.INVISIBLE);              // set login button invisible
+//            //signupButton.setVisibility(View.INVISIBLE);             // set signup button invisible
+//        }
 
         //Moves to login screen automatically
         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(intent);
-
-
+    }}
 //
-    }
-    private void CreatePin(){
-
-    }
-}
-
+////
+//    }
+//    private void CreatePin(){
+//
+//    }
+//}
+//
 
 /* PSEUDOCODE
 oncreate
