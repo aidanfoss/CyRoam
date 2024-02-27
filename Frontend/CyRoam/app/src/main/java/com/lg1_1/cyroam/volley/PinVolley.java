561f2e162b9274b1300d0deb15eae237f45faa57
Aidan Foss <quantumaidan@gmail.com>
2024-02-26 22:20:31 -0600

    might be useful later. useless ATM

---
+++ Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/volley/PinVolley.java
@@ -0,0 +1,16 @@
+//package com.lg1_1.cyroam.volley;
+//
+//import android.content.Context;
+//
+//import com.android.volley.RequestQueue;
+//import com.android.volley.toolbox.Volley;
+//
+//public class PinVolley extends Context {
+//    private RequestQueue mQueue;
+//    protected void CallPin(int id){
+//        mQueue = Volley.newRequestQueue(this);
+//    }
+//}
+
+
+//maybe this might be a convenient way of doing this later. For now, i might have to call each thing individually.
