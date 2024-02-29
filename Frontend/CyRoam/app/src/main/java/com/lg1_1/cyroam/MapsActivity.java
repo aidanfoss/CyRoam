714d221f4ac4a5b2b89d805ff7793f0d397e3996
Aidan Foss <quantumaidan@gmail.com>
2024-02-28 23:54:47 -0600

    working on implementing current location as a LatLng

--- Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/MapsActivity.java
+++ Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/MapsActivity.java
@@ -1,15 +1,21 @@
 package com.lg1_1.cyroam;
 
+import com.lg1_1.cyroam.util.Pin;
 import static com.lg1_1.cyroam.MainActivity.url;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.FrameLayout;
+import android.Manifest;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -22,7 +28,6 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.lg1_1.cyroam.util.Pin;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -30,7 +35,8 @@ import org.json.JSONObject;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import java.util.Vector;
+
+import java.util.Objects;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -38,14 +44,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private RequestQueue mQueue; // define volley request queue
     private FloatingActionButton newPinButton; // define new pin button variable
     private FloatingActionButton discoverButton; //define discoverButton
-//    Vector<Pin> pinVector = new Vector<>(); not necessary, commented out to fix errors
-
 
     //TODO define user object here to determine what they can and cant do
     //this can also change what does and doest display (ex:no fog on admin account, no distance limit etc)
 
+    //location & locationPermissions things
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1001;
     private FusedLocationProviderClient fusedLocationClient;
+    private Location lastKnownLocation;
+    private Boolean locationPermissionGranted;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
 
 
     GoogleMap gMap;
@@ -57,11 +65,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps); //Link to XML
 
         mQueue = Volley.newRequestQueue(this); //defines volley queue for fillPinVector
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+            return;
+        }
 
 
+        //define and implement buttons
         newPinButton = findViewById(R.id.newPinButton);
         discoverButton = findViewById(R.id.discoverButton);
-
         newPinButton.setOnClickListener(v -> {
             Intent intent = new Intent(MapsActivity.this, NewPinActivity.class);
             startActivity(intent);  // go to NewPinActivity
@@ -85,27 +103,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     //https://github.com/googlemaps-samples/android-samples/blob/588287af6ea872c6098c2b4c727503200de4dc7e/tutorials/java/CurrentPlaceDetailsOnMap/app/src/main/java/com/example/currentplacedetailsonmap/MapsActivityCurrentPlace.java#L267-L282
 
 
-//    private void updateLocationUI() { //got this chunk of code from https://developers.google.com/maps/documentation/android-sdk/current-place-tutorial#java_4
-//        //changed map call to gMap call, not sure how to check for locationPermissionGranted or lastKnownLocation just yet.
-//        //did i miss something in the documentation? This entire function is in the wrong spot.
-//        if (gMap == null) {
-//            return;
-//        }
-//        try {
-//            boolean locationPermissionGranted = true; //temp solution?
-//            if (locationPermissionGranted) {
-//                gMap.setMyLocationEnabled(true);
-//                gMap.getUiSettings().setMyLocationButtonEnabled(true);
-//            } else {
-//                gMap.setMyLocationEnabled(false);
-//                gMap.getUiSettings().setMyLocationButtonEnabled(false);
-//                lastKnownLocation = null;
-//                getLocationPermission();
-//            }
-//        } catch (SecurityException e)  {
-//            Log.e("Exception: %s", Objects.requireNonNull(e.getMessage()));
-//        }
-//    } //This is fucked atm. Not sure how to check for locationPermissionGranted yet.
+    private void updateLocationUI() { //got this chunk of code from https://developers.google.com/maps/documentation/android-sdk/current-place-tutorial#java_4
+        if (gMap == null) {
+            return;
+        }
+        try {
+            if (locationPermissionGranted) {
+                gMap.setMyLocationEnabled(true);
+                gMap.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                gMap.setMyLocationEnabled(false);
+                gMap.getUiSettings().setMyLocationButtonEnabled(false);
+                lastKnownLocation = null;
+                getLocationPermission();
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", Objects.requireNonNull(e.getMessage()));
+        }
+    }
+
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
