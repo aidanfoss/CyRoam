a44d5b8394670658220a7e0d08c9cf804cdd8d00
Zach Merwin <merwi002@iastate.edu>
2024-03-04 17:36:24 -0600

    fixed userCheck to work with frontend

--- Backend/CyRoam Backend/Main/src/main/java/coms309/Application.java
+++ Backend/CyRoam Backend/Main/src/main/java/coms309/Application.java
@@ -26,51 +26,6 @@ public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
-    @Bean
-    CommandLineRunner initUser(UserInterface userInterface) {
-        return args -> {
 
-            User user1 = new User("bossf", "123");
-            User user2 = new User("Jane", "jane@somemail.com");
-
-            userInterface.save(user1);
-            userInterface.save(user2);
-
-        };
-    }
-    @Bean
-    CommandLineRunner initFriend(FriendObjInterface friendObjInterface) {
-        return args -> {
-            FriendObj friendObj1 = new FriendObj("bossf", "zach");
-            FriendObj friendObj2 = new FriendObj("Jane", "poop");
-            FriendObj friendObj3 = new FriendObj("bossf", "stinky");
-
-            friendObjInterface.save(friendObj1);
-            friendObjInterface.save(friendObj2);
-            friendObjInterface.save(friendObj3);
-        };
-    }
-
-    @Bean
-    CommandLineRunner initPin(PinRepository pinRepository) {
-        return args -> {
-            Pin zerozero = new Pin(0.005, 0.0, "Zero Zero Error Point");
-            pinRepository.save(zerozero);
-            Pin parkslibrary = new Pin(42.0281254, -93.6678685, "Parks Library");
-            pinRepository.save(parkslibrary);
-            Pin aidanapartment = new Pin(42.0351026, -93.6425714, "Aidan's Apartment");
-            pinRepository.save(aidanapartment);
-        };
-    }
-
-    @Bean
-    CommandLineRunner initProgress(ProgressRepository progressRepository) {
-        return args -> {
-            Progress example = new Progress(1, 1, false);
-            Progress example2 = new Progress(2, 1, false);
-            progressRepository.save(example);
-            progressRepository.save(example2);
-        };
-    }
 
 }
