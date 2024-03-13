9f9bab9ced7dc16e56e191c63af88aeb53910e23
Nick127k <Nick127k@iastate.edu>
2024-03-13 13:31:56 -0500

    new addfriend screen

---
+++ Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/AddFriends.java
@@ -0,0 +1,14 @@
+package com.lg1_1.cyroam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddFriends extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_friends);
+    }
+}
