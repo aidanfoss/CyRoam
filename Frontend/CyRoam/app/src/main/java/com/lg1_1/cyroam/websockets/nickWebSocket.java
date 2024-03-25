3ae2b89c52121e2eb32b8e2b26c43bce726f17a8
Aidan Foss <quantumaidan@gmail.com>
2024-03-25 15:36:12 -0500

    fixed duplicate functions i wrote to fix issue on my branch being merged in

--- Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/websockets/nickWebSocket.java
+++ Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/websockets/nickWebSocket.java
@@ -1,41 +1,19 @@
 package com.lg1_1.cyroam.websockets;
 
 import android.content.Context;
+import android.util.Log;
 
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
+import org.json.JSONObject;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 
 public class nickWebSocket extends WebSocketClient {
 
     private final String TAG = "Nick Websocket";
-    private WebSocketListener webSocketListener;
-
-    public nickWebSocket(String serverUri, WebSocketListener listener) throws URISyntaxException {
-        super(new URI(serverUri));
-        this.webSocketListener = listener;
-    }
-
-    @Override
-    public void onOpen(ServerHandshake handshakedata) {
-
-    }
-
-    @Override
-    public void onMessage(String message) {
-
-    }
-
-    @Override
-    public void onClose(int code, String reason, boolean remote) {
-
-    }
-
-    @Override
-    public void onError(Exception ex) {
-
-    }
+    private final WebSocketListener webSocketListener;
     //do alt enter to fix the bug above. It'll autofill for you each required function
     //do a lot of Log.v(TAG, "info")'s
     //look at my onMessage class, implement the onFriendRequestRecieved interface
