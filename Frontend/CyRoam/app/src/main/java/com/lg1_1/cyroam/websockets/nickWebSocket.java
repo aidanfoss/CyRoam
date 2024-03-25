2073a5d7878ea39b56a2879dde22d85f7e41e039
Aidan Foss <quantumaidan@gmail.com>
2024-03-25 15:41:34 -0500

    fixed multiple warnings (typos, etc)

--- Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/websockets/nickWebSocket.java
+++ Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/websockets/nickWebSocket.java
@@ -1,6 +1,5 @@
 package com.lg1_1.cyroam.websockets;
 
-import android.content.Context;
 import android.util.Log;
 
 import org.java_websocket.client.WebSocketClient;
@@ -16,7 +15,7 @@ public class nickWebSocket extends WebSocketClient {
     private final WebSocketListener webSocketListener;
     //do alt enter to fix the bug above. It'll autofill for you each required function
     //do a lot of Log.v(TAG, "info")'s
-    //look at my onMessage class, implement the onFriendRequestRecieved interface
+    //look at my onMessage class, implement the onFriendRequestReceived interface
     //i have the interface at the bottom of mapsActivity for you.
 
     public nickWebSocket(String serverUri2, WebSocketListener listener) throws URISyntaxException {
@@ -31,7 +30,7 @@ public class nickWebSocket extends WebSocketClient {
 
     @Override
     public void onMessage(String name) {
-        Log.v(TAG, "message recieved: " + name);
+        Log.v(TAG, "message received: " + name);
         //hopefully the message will be a JSON
         try {
             //convert the message into a json object
