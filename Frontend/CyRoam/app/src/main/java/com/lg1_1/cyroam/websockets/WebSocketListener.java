466fe1d6484594027e04d8ac120a75ac5284b6b8
Aidan Foss <quantumaidan@gmail.com>
2024-03-27 19:07:28 -0500

    javadoc

--- Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/websockets/WebSocketListener.java
+++ Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/websockets/WebSocketListener.java
@@ -1,5 +1,9 @@
 package com.lg1_1.cyroam.websockets;
 
+/**
+ * Listener that is implemented in the maps function
+ * allows easy websocket handling
+ */
 public interface WebSocketListener {
     void onPinRecieved(int id);
 
--- Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/websockets/aidanWebSocket.java
+++ Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/websockets/aidanWebSocket.java
@@ -2,6 +2,7 @@ package com.lg1_1.cyroam.websockets;
 import android.util.Log;
 
 import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.framing.CloseFrame;
 import org.java_websocket.handshake.ServerHandshake;
 import org.json.JSONObject;
 
@@ -11,16 +12,34 @@ import java.net.URISyntaxException;
 public class aidanWebSocket extends WebSocketClient{
     private final String TAG = "Aidan Websocket";
     private WebSocketListener webSocketListener;
+
+    /**
+     * @author Aidan Foss
+     * constructor for pins websocket
+     * @param serverUri
+     * @param listener
+     * @throws URISyntaxException
+     */
     public aidanWebSocket(String serverUri, WebSocketListener listener) throws URISyntaxException{
         super(new URI(serverUri));
         this.webSocketListener = listener;
     }
 
+    /**
+     * @author Aidan Foss
+     * logs connection in logcat, can handle other things in the future
+     * @param handshakedata The handshake of the websocket instance
+     */
     @Override
     public void onOpen(ServerHandshake handshakedata) {
         Log.v(TAG, "connected");
     }
 
+    /**
+     * @author Aidan Foss
+     * activates WebSocketListener to be used in MapsActivity on message recieved
+     * @param message The UTF-8 decoded message that was received.
+     */
     @Override
     public void onMessage(String message) {
         Log.v(TAG, "message received: " + message);
@@ -35,11 +54,26 @@ public class aidanWebSocket extends WebSocketClient{
         }
     }
 
+    /**
+     * @author Aidan Foss
+     * Logs a closed connection, along with reason
+     * @param code
+     *            The codes can be looked up here: {@link CloseFrame}
+     * @param reason
+     *            Additional information string
+     * @param remote
+     *            Returns whether or not the closing of the connection was initiated by the remote host.
+     */
     @Override
     public void onClose(int code, String reason, boolean remote) {
-        Log.v(TAG, "connection closed");
+        Log.v(TAG, "connection closed: " + reason);
     }
 
+    /**
+     * @author Aidan Foss
+     * Logs any websocket related errors
+     * @param ex The exception causing this error
+     */
     @Override
     public void onError(Exception ex) {
         Log.w(TAG, "aidanWebSocket error: " + ex.toString());
