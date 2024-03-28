1ef19b91289391f36e5e2bcfa474581e18e10382
Aidan Foss <quantumaidan@gmail.com>
2024-03-27 20:30:44 -0500

    javadoc gradle update

--- Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/volley/progressVolley.java
+++ Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/volley/progressVolley.java
@@ -14,6 +14,10 @@ import com.lg1_1.cyroam.MainActivity;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+/**
+ * Handles volley requests specifically for pin data.
+ * @author Aidan Foss
+ */
 public class progressVolley {
 
     private static final String TAG = "progressVolley";
@@ -21,13 +25,25 @@ public class progressVolley {
     private RequestQueue queue;
     private Context context;
 
+    /**
+     * Constructor for the progressVolley object, allowing other classes to call
+     * functions defined in this class.
+     * @param context should always be constructed with "this"
+     */
     public progressVolley(Context context){
         this.context = context;
         queue = Volley.newRequestQueue(context); //tutorial said to do this, but not sure why. couldn't it just be (this) for context?
     }
 
-    /*
-    ths function creates a new progressobject which logs discovery of a pin for a certain player.
+    /**
+     * this function creates a new progress object
+     * which logs discovery of a pin for a given player.
+     * Uses volley to store that information in the database
+     *
+     * @author Aidan Foss
+     * @param userId the user ID of the player
+     * @param pinId the pin ID of the pin they are discovering
+     * @param callback callback that handles errors and passes information
      */
     public void discoverPin(int userId, int pinId, final VolleyCallback callback){
 
@@ -61,11 +77,21 @@ public class progressVolley {
                 });
         queue.add(request);
     }
+
+    /**
+     * Handles errors and passes information when discovering pins
+     * @author Aidan Foss
+     */
     public interface VolleyCallback {
         void onSuccess(boolean discovered);
         void onFailure(String errorMessage);
     }
 
+    /**
+     * @deprecated because we've moved away from individual progress objects for each user and pin.
+     * @param progressId the ID of the progress object stored in the server.
+     * @param callback handles errors and passes information
+     */
     public void fetchProgress(int progressId, final VolleyCallbackGet callback){
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, BASE_URL + "/progress/" + String.valueOf(progressId), null,
                 response -> {
@@ -87,6 +113,11 @@ public class progressVolley {
         queue.add(request);
     }
 
+    /**
+     * @deprecated
+     * Handles errors and passes information when calling pin discovery information
+     * @author Aidan Foss
+     */
     public interface VolleyCallbackGet {
         void onSuccess(int pinId, int userId, boolean discovered, int progressObjId);
         void onFailure(String errorMessage);
