96279c9cdf45fd7c5131ee4a9fd8f096a0c397c4
Aidan Foss <quantumaidan@gmail.com>
2024-04-02 16:39:11 -0500

    oops 39b

--- Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/websockets/WebSocketListener.java
+++ Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/websockets/WebSocketListener.java
@@ -1,12 +1,13 @@
 package com.lg1_1.cyroam.websockets;
 
+import org.json.JSONObject;
+
 /**
  * Listener that implements WebSocket Handling
  * @author Aidan Foss
  */
 public interface WebSocketListener {
     void onPinRecieved(int id);
-
-
+    void onCommentReceived(String comment);
     void onfredReqRecieved(String name);
 }
