477bc554b7c34faa5eae88030ac946cb5fdcf88f
Nick127k <Nick127k@iastate.edu>
2024-04-13 15:14:04 -0500

    Im cooking on the final sign up screen

---
+++ Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/AdminLeaderBoardControl.java
@@ -0,0 +1,14 @@
+package com.lg1_1.cyroam;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminLeaderBoardControl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_leader_board_control);
+    }
+}
