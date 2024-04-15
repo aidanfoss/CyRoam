e523d1cd383e4db50437d633a5ec3319a49bc1e6
Aidan Foss <quantumaidan@gmail.com>
2024-04-15 16:58:23 -0500

    test3

--- Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/MainActivity.java
+++ Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/MainActivity.java
@@ -11,7 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
  * Automatically transitions you to the login activity.
  */
 public class MainActivity extends AppCompatActivity {
-	//test3
+	//test4
 
     public static final String url = "http://coms-309-008.class.las.iastate.edu:8080";
     public static final String wsurl = "ws://coms-309-008.class.las.iastate.edu:8080";
