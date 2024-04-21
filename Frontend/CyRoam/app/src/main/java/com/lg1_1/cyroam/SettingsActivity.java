8bebad56a539937a5a8b42168f7153fd43bcef95
Nick127k <Nick127k@iastate.edu>
2024-04-21 14:11:02 -0500

    Setting Screen Set up 2nd activity stuff

--- Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/SettingsActivity.java
+++ Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/SettingsActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -36,6 +37,10 @@ public class SettingsActivity extends AppCompatActivity {
 
     private Button removeButton;
 
+    private Button leaderboardPermissionButton;
+
+    private TextView OutputText;
+
     private EditText removefriend;
 
     private RequestQueue queue;
@@ -54,6 +59,8 @@ public class SettingsActivity extends AppCompatActivity {
         //backbuttontoportal = findViewById(R.id.settingsbackbutton);
         removeButton =  findViewById(R.id.buttonRemoveFriend);
         removefriend = findViewById(R.id.textViewRemoveFriend);
+        leaderboardPermissionButton = findViewById(R.id.buttonRemoveSelfFromLeaderBoard);
+        OutputText = findViewById(R.id.settingScreenOutputText);
         /*
         backbuttontoportal.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,10 +75,32 @@ public class SettingsActivity extends AppCompatActivity {
         removeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /* when signup button is pressed, use intent to switch to Signup Activity */
+                /* This removes the fridn that is in the EditText */
                 String curUsername = "bossf";
                 String noLongerfriend = removefriend.getText().toString();
                 removefriend(curUsername, noLongerfriend);
+
+            }
+        });
+        /**
+         * The user can choose whether they appear on the leaderboard
+         */
+        leaderboardPermissionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username ="er";
+               leaderBoardReq(username, new LoginActivity.VolleyCallback() {
+                    @Override
+                    public void onSuccess(boolean isTrue) {
+                        OutputText.setText("You now dont show on the leader board");
+                    }
+
+                    @Override
+                    public void onFailure(String errorMessage) {
+                        Log.e(TAG, "make string req onFailure " + errorMessage);
+                    }
+
+                });
 
             }
         });
@@ -80,50 +109,21 @@ public class SettingsActivity extends AppCompatActivity {
 
 
 
-
-
-
+    /**
+     * This removes friends from users friend list
+     * @author Nicholas Kirschbaum
+     */
     private void removefriend(String curUsername,String Newfriend){
         String url = mainURL + "/deleteFriend/" + curUsername + "/" + Newfriend;
 
-        // Convert input to JSONObject
-        /*JSONObject userInfo = new JSONObject();
-        try{
-
-            // etRequest should contain a JSON object string as your POST body
-            // similar to what you would have in POSTMAN-body field
-            // and the fields should match with the object structure of @RequestBody on sb
-            userInfo.put("curUsername", curUsername);
-            userInfo.put("friendUsername", Newfriend);
-
-
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-        */
         JsonObjectRequest request = new JsonObjectRequest(
                 Request.Method.DELETE,
                 url,
                 null,
                 response -> {
                     try{
-
-
                         JSONArray jsonArray = response.getJSONArray("deleteFriend");
                         Log.i(TAG, "request success");
-                        //outputtext.setText(curUsername + " Friends:\n");
-                        // for (int i = 0; i < jsonArray.length(); i++){
-                        //  JSONObject friend = jsonArray.getJSONObject(i);
-
-                        //  String curUser = friend.getString("curUsername");
-                        // String friendUser = friend.getString("friendUsername");
-                        // output = curUser + " " + friendUser;
-
-                        //outputtext.append(friendUser + "\n");
-                        //Log.i(TAG, output);
-
-
-                        //}
 
                     }catch (JSONException e){
                         e.printStackTrace();
@@ -144,21 +144,73 @@ public class SettingsActivity extends AppCompatActivity {
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
                 HashMap<String, String> headers = new HashMap<String, String>();
-                //                headers.put("Authorization", "Bearer YOUR_ACCESS_TOKEN");
-                //                headers.put("Content-Type", "application/json");
                 return headers;
             }
-
             @Override
             protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<String, String>();
-                //                params.put("param1", "value1");
-                //                params.put("param2", "value2");
                 return params;
             }
+        };
+        // Adding request to request queue
+        queue.add(request);
+    }
+
+    /**
+     * get request to login with the username and password they put in
+     * @author Nicholas Kirschbaum
+     */
+    private void leaderBoardReq(String curUsername, final LoginActivity.VolleyCallback callback){
+        //private void makeStringReq(String curUsername, String password){
+        String url = mainURL + "/userCheck/" + curUsername;
+
+
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                //userInfo2,
+                // so the issue is that its saying that there is an error
+                //Log.v(TAG, "respo success: "),
+                response -> {
+                    Log.v(TAG, "respo success: " + response.toString());
+                    try{
+                        Log.v(TAG, "request success");
+                        boolean isTrue = response.getBoolean("isUser");
+                        int i = 0;
+                        //inputuser = new User(curUsername, password, i);
+
+
+                        //return isTrue;
+                        callback.onSuccess(isTrue);
+
+                    }catch (JSONException e){
+                        Log.e(TAG, "catch error: " + e.getMessage());
+                        e.printStackTrace();
+                        callback.onFailure(e.getMessage());
+                    }
+
+                    // output = response.toString();
+
+                },
+                error -> {
+                    Log.e(TAG, "error lambda error: " + error.getMessage());
+                    // tvResponse.setText(error.getMessage());
+                }
+
+        ){
+//
         };
 
         // Adding request to request queue
         queue.add(request);
+//        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
+    }
+    /**
+     *Volley callback for Log in
+     */
+    public interface VolleyCallback{
+        void onSuccess(boolean isTrue);
+        void onFailure(String errorMessage);
     }
 }
