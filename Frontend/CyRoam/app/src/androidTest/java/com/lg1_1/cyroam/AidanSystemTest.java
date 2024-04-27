9cc04467af198c7c5ddf15ded07921687b4902dc
Aidan Foss <quantumaidan@gmail.com>
2024-04-26 20:36:13 -0500

    Fixed all issues, made a few test cases

--- Frontend/CyRoam/app/src/androidTest/java/com/lg1_1/cyroam/AidanSystemTest.java
+++ Frontend/CyRoam/app/src/androidTest/java/com/lg1_1/cyroam/AidanSystemTest.java
@@ -3,9 +3,10 @@ package com.lg1_1.cyroam;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -21,7 +22,9 @@ public class AidanSystemTest {
             = new ActivityScenarioRule<>(MapsActivity.class);
     @Test
     public void testNavigation(){
-        onView(withId(R.id.portalButton)).check(matches(isDisplayed())).perform(click());
-        onView(withId(R.id.portalButton1)).check(matches(isDisplayed())).perform(click());
+        onView(withId(R.id.portalButton)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withId(R.id.portalButton)).perform(click());
+        onView(withId(R.id.portalButton1)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withId(R.id.portalButton1)).perform(click());
     }
 }
