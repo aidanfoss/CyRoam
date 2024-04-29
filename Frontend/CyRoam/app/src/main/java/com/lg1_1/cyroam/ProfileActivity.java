9f3c280a1615a6975b051722782a54864a591c23
Nick127k <Nick127k@iastate.edu>
2024-04-29 15:31:03 -0500

    Working on the profile somewhat cooking

--- Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/ProfileActivity.java
+++ Frontend/CyRoam/app/src/main/java/com/lg1_1/cyroam/ProfileActivity.java
@@ -1,10 +1,15 @@
 package com.lg1_1.cyroam;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 /**
  * This shows the Users basic information and basic achievements
@@ -17,17 +22,20 @@ public class ProfileActivity extends AppCompatActivity {
 
     private Button backButton;
 
+    private RequestQueue queue;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        queue = Volley.newRequestQueue(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        //ConstraintLayout constraintLayout = findViewById(R.id.mainlayout);
-        //AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
-        //animationDrawable.setEnterFadeDuration(2500);
-        //animationDrawable.setExitFadeDuration(5000);
-        //animationDrawable.start();
+        ConstraintLayout constraintLayout = findViewById(R.id.mainlayoutforProfile);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
         //backButton = findViewById(R.id.backButtonProfileScreen);
         //scoreOutput = findViewById(R.id.textViewScoreOutput);
         //pinOutput = findViewById(R.id.textViewPinsCount);
